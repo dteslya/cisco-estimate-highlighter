efc7e8e526434fd90bd50b51e27e68bd78731929
Dmitriy Teslya <dteslya@Dmitriys-MacBook-Pro.local>
2020-07-24 21:58:56 +0300

    react build

---
+++ client/build/precache-manifest.c13cb8505870584ee0e47ac6c6b8d737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362980e06d2c497fc9e4026b639753e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2efc0ad6ea50e8a22bc9",
+    "url": "/static/css/2.c301f1a7.chunk.css"
+  },
+  {
+    "revision": "2efc0ad6ea50e8a22bc9",
+    "url": "/static/js/2.151b995b.chunk.js"
+  },
+  {
+    "revision": "ad7fe4e7f743297df117e0e71b597918",
+    "url": "/static/js/2.151b995b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "215b48319eb324227050",
+    "url": "/static/js/main.b61b5830.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
