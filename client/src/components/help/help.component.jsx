350ac0a34d14662c809e40f4995beed90db5e96c
Dmitriy Teslya <dteslya@Dmitriys-MacBook-Pro.local>
2020-07-28 16:18:53 +0300

    -nY- string update

--- client/src/components/help/help.component.jsx
+++ client/src/components/help/help.component.jsx
@@ -14,6 +14,7 @@ Upload CCW estimate in XLSx format. Resulting file will be downloaded automatica
         
 ## Matching criteria
 * Part Numbers starting with **R-**, **L-**, **S-**, **LIC-** or ending with **Y** and **Price > $0**
+* Part Numbers containing **-nY-** and **Price > $0** where **n** is in range from 1 to 9
 * Description contains **eDelivery** and **Price > $0**
 * **Lead time <= 10 days** and **Price > $0**`}
       </Markdown>
